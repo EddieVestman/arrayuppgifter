3dedcbabf9a5fa2e0a72b929f52cd2d055acb9c3
EddieVestman <edve02001@utb.vaxjo.se>
2019-10-04 09:19:27 +0200

    commit

--- array2/array2/Program.cs
+++ array2/array2/Program.cs
@@ -6,7 +6,15 @@ namespace array2
     {
         static void Main(string[] args)
         {
-            
+            double[] temp = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
+            double sum = 0;
+            for (int i = 0; i < temp.Length; i++)
+            {
+                sum += temp[i];
+            }
+            double average = sum / temp.Length;
+            Console.WriteLine(average);
+
         }
     }
 }
