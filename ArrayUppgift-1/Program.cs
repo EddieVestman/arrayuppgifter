4459e2062353f98006e73caa03526413e1ac7e8b
EddieVestman <edve02001@utb.vaxjo.se>
2019-10-04 08:39:21 +0200

    la till filerna i rätt map

---
+++ ArrayUppgift-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArrayUppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] t = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Vänligen skriv in 1 heltal: ");
+                t[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i = t.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine("Nr: " + t[i]);
+
+            }
+        }
+    }
+}
